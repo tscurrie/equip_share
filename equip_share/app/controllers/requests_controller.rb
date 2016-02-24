c8ce8a7438b2eca760b3e8bc47cee991a32fb878
tscurrie <tscurrie85@gmail.com>
2016-02-24 10:27:58 -0800

    added incoming and outgoing request page

--- equip_share/app/controllers/requests_controller.rb
+++ equip_share/app/controllers/requests_controller.rb
@@ -1,21 +1,28 @@
 class RequestsController < ApplicationController
 	def new
-		@item = Item.find_by_id(params[:id])
-		@request = @item.requests.new
+		@item = Item.find(params[:item_id])
+		@request = Request.new
 	end
 	def create
-		@item = Item.find_by_id(params[:id])
+		@item = Item.find(params[:item_id])
 		request_params = params.require(:request).permit(:start_date, :end_date)
 		request_params[:borrower_id] = current_user.id
-		request_params[:item_id] = @item.to_i
-		request_params[:user_id] = 
-		request = Request.new(request_params)
+		request_params[:item_id] = @item
+		request_params[:user_id] = @item.user.id
+		@request = Request.new(request_params)
 
-		if request.save
-			redirect_to new_request_path
+		if @request.save
+			@item.requests.push(@request)
+			redirect_to user_path(current_user)
 		end
 	end
 
+	def incoming
+		@user = User.find(params[:id])
+		@item = Item.find(params[:id])
+		@incoming = Request.where(user_id: @user)
+		@outgoing = Request.where(borrower_id: @user)
+	end
 
 
 end
