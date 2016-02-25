f8b652f9a783745532b53a0bf5a2d4e64b45e7e4
tscurrie <tscurrie85@gmail.com>
2016-02-25 00:32:11 -0800

    added jumbotron with tint

--- equip_share/app/models/user.rb
+++ equip_share/app/models/user.rb
@@ -7,4 +7,13 @@ class User < ActiveRecord::Base
 		@user = User.find_by({email: params[:email]})
 		@user.try(:authenticate, params[:password])
 	end
+
+	validates :first_name, :last_name, :email, :password, presence: true, length: {maximum: 255}
+	validates :password, length: { minimum: 3, message: "must be at least 3 characters" }
+	validates :email, uniqueness: true
+
+	validates :email, format: { with: /@/ }, length: { minimum: 6 }
+
+
+
 end
