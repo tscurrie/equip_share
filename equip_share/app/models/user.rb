87ede01dd68b070a0279ef25f759ad56c7ccac4e
tscurrie <tscurrie85@gmail.com>
2016-03-08 18:19:57 -0800

    fixed add item

--- equip_share/app/models/user.rb
+++ equip_share/app/models/user.rb
@@ -1,24 +1,26 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	has_many :items, :dependent => :delete_all
+	has_many :requests, through: :items
 
 
-def self.confirm(params)
-	@user = User.find_by({email: params[:email]})
-	@user.try(:authenticate, params[:password])
+	def self.confirm(params)
+	    @user = User.find_by({email: params[:email]})
+	    @user.try(:authenticate, params[:password])
 
-end
-validates :email,
-presence: true,
-uniqueness: true,
-format: { with: /@/ },
-length: { minimum: 6, message: 'Must be at least 6 characters and include an @ symbol'  }
+	  end
+	  validates :email,
+	  presence: true,
+	  uniqueness: true,
+	  format: { with: /@/ },
+	  length: { minimum: 6, message: 'Must be at least 6 characters and include an @ symbol'  }
 
-# validates :password,
-# presence: true,
-# length: { minimum: 6, message: 'Must be at least 6 characters in length' }
+	  # validates :password,
+	  # presence: true,
+	  # length: { minimum: 6, message: 'Must be at least 6 characters in length' }
 
-validates :first_name, presence: true
+	  validates :first_name, presence: true
 
-validates :last_name, presence: true
+	  validates :last_name, presence: true
 
 end
