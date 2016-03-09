92842746c0311c1afef6c8ba1a37a0f3790ba83d
tscurrie <tscurrie85@gmail.com>
2016-03-08 18:35:33 -0800

    made request dependent on items so now delete works

--- equip_share/app/models/item.rb
+++ equip_share/app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
 	belongs_to :user
-	has_many :requests
+	has_many :requests, :dependent => :delete_all
 
 	# has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
 
